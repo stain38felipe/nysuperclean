b7a18ec5292a629e98b2ad36571da731954748e9
POSITIVO\stain <hfelipe.sanchez@gmail.com>
2025-02-10 18:51:36 -0500

    Revert "Merge branch 'development' of https://github.com/stain38felipe/nysuperclean"
    This reverts commit ad3305642448d787d9bc639e6872e4468e55de58, reversing
    changes made to d61f055f77d1ff042026aa01e4a221df47157701.

--- wp-content/themes/nysc/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <p>*All content and materials on the Site, including but not limited to text, graphics, logos, and images, are the property of NY Super Clean or its content
-    suppliers and are protected by intellectual property laws. You may not use any content from the Site without our express written permission.</p>
-    <?php wp_footer(); ?>
-</footer>
-</body>
-</html>
