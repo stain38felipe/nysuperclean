e99e0b211d2d00d484a79395ddfd91436586befe
David Millan <davide.millan@gmail.com>
2025-02-17 17:47:53 -0500

    :lipstick: Se dejan todas las imágenes dinámicas y css arreglado para desktop para 1600, 1440 y 1024 px deancho :lipstick:

--- wp-content/themes/nysc/functions.php
+++ wp-content/themes/nysc/functions.php
@@ -44,6 +44,38 @@ function agregar_estilos_dinamicos() {
     $imagen_medium_logo = wp_get_attachment_image_src($imagen_id_logo, 'medium')[0];
     $imagen_full_logo = wp_get_attachment_image_src($imagen_id_logo, 'full')[0];
 
+    $imagen_id_au = 89;
+    $imagen_thumbnail_au = wp_get_attachment_image_src($imagen_id_au, 'thumbnail')[0];// 150
+    $imagen_full_au = wp_get_attachment_image_src($imagen_id_au, 'full')[0];
+    $imagen_medium_au = wp_get_attachment_image_src($imagen_id_au, 'medium')[0]; //300
+    $imagen_medium_large_au = wp_get_attachment_image_src($imagen_id_au, 'medium_large')[0];//768
+    $imagen_large_au = wp_get_attachment_image_src($imagen_id_au, 'large')[0]; //1024
+    $imagen_1536_au = wp_get_attachment_image_src($imagen_id_au, '1536x1536')[0]; // 1536px width
+
+    $imagen_id_ser = 71;
+    $imagen_thumbnail_ser = wp_get_attachment_image_src($imagen_id_ser, 'thumbnail')[0];// 150
+    $imagen_full_ser = wp_get_attachment_image_src($imagen_id_ser, 'full')[0];
+    $imagen_medium_ser = wp_get_attachment_image_src($imagen_id_ser, 'medium')[0]; //300
+    $imagen_medium_large_ser = wp_get_attachment_image_src($imagen_id_ser, 'medium_large')[0];//768
+    $imagen_large_ser = wp_get_attachment_image_src($imagen_id_ser, 'large')[0]; //1024
+    $imagen_1536_ser = wp_get_attachment_image_src($imagen_id_ser, '1536x1536')[0]; // 1536px width
+
+    $imagen_id_os = 94;
+    $imagen_thumbnail_oºs = wp_get_attachment_image_src($imagen_id_os, 'thumbnail')[0];// 150
+    $imagen_full_os = wp_get_attachment_image_src($imagen_id_os, 'full')[0];
+    $imagen_medium_os = wp_get_attachment_image_src($imagen_id_os, 'medium')[0]; //300
+    $imagen_medium_large_os = wp_get_attachment_image_src($imagen_id_os, 'medium_large')[0];//768
+    $imagen_large_os = wp_get_attachment_image_src($imagen_id_os, 'large')[0]; //1024
+    $imagen_1536_os = wp_get_attachment_image_src($imagen_id_os, '1536x1536')[0]; // 1536px width
+
+    $imagen_id_lc = 120;
+    $imagen_thumbnail_lc = wp_get_attachment_image_src($imagen_id_lc, 'thumbnail')[0];// 150
+    $imagen_full_lc = wp_get_attachment_image_src($imagen_id_lc, 'full')[0];
+    $imagen_medium_lc = wp_get_attachment_image_src($imagen_id_lc, 'medium')[0]; //300
+    $imagen_medium_large_lc = wp_get_attachment_image_src($imagen_id_lc, 'medium_large')[0];//768
+    $imagen_large_lc = wp_get_attachment_image_src($imagen_id_lc, 'large')[0]; //1024
+    $imagen_1536_lc = wp_get_attachment_image_src($imagen_id_lc, '1536x1536')[0]; // 1536px width
+
     $css = "
         @media (min-width: 300px) {
             .hero-section {
@@ -51,6 +83,18 @@ function agregar_estilos_dinamicos() {
             }
             .hero-logo, .page-logo {
                 background-image: url('{$imagen_medium_logo}');
+            }
+            .about-us-section {
+                background-image: url('{$imagen_medium_au}');
+            }
+            .service-section {
+                background-image: url('{$imagen_medium_ser}');
+            }
+            .our-staff-section {
+                background-image: url('{$imagen_medium_os}');
+            }
+            .lets-connect-section {
+                background-image: url('{$imagen_medium_lc}');
             }
         }
         @media (min-width: 600px) {
@@ -60,6 +104,18 @@ function agregar_estilos_dinamicos() {
             .hero-logo, .page-logo {
                 background-image: url('{$imagen_medium_logo}');
             }
+            .about-us-section {
+                background-image: url('{$imagen_medium_large_au}');
+            }
+            .service-section {
+                background-image: url('{$imagen_medium_large_ser}');
+            }
+            .our-staff-section {
+                background-image: url('{$imagen_medium_large_os}');
+            }
+            .lets-connect-section {
+                background-image: url('{$imagen_medium_large_lc}');
+            }
         }
         @media (min-width: 1024px) {
             .hero-section {
@@ -68,6 +124,18 @@ function agregar_estilos_dinamicos() {
             .hero-logo, .page-logo {
                 background-image: url('{$imagen_full_logo}');
             }
+            .about-us-section {
+                background-image: url('{$imagen_large_au}');
+            }
+            .service-section {
+                background-image: url('{$imagen_large_ser}');
+            }
+            .our-staff-section {
+                background-image: url('{$imagen_large_os}');
+            }
+            .lets-connect-section {
+                background-image: url('{$imagen_large_lc}');
+            }
         }
         @media (min-width: 1440px) {
             .hero-section {
@@ -75,6 +143,18 @@ function agregar_estilos_dinamicos() {
             }
             .hero-logo, .page-logo {
                 background-image: url('{$imagen_full_logo}');
+            }
+            .about-us-section {
+                background-image: url('{$imagen_1536_au}');
+            }
+            .service-section {
+                background-image: url('{$imagen_1536_ser}');
+            }
+            .our-staff-section {
+                background-image: url('{$imagen_1536_os}');
+            }
+            .lets-connect-section {
+                background-image: url('{$imagen_1536_lc}');
             }
         }
         @media (min-width: 1600px) {
@@ -86,6 +166,18 @@ function agregar_estilos_dinamicos() {
             }
             .hero-logo, .page-logo {
                 background-image: url('{$imagen_full_logo}');
+            }
+            .about-us-section {
+                background-image: url('{$imagen_full_au}');
+            }
+            .service-section {
+                background-image: url('{$imagen_full_ser}');
+            }
+            .our-staff-section {
+                background-image: url('{$imagen_full_os}');
+            }
+            .lets-connect-section {
+                background-image: url('{$imagen_full_lc}');
             }
         }
     ";
